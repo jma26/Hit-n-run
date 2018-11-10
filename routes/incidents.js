d8a52dccce8c6b87e6bc37966abbe0a257a97cc7
Dennis Onder <webdev.dennisonder@gmail.com>
2018-11-10 15:20:11 +0100

    Incident route prepared

--- routes/incidents.js
+++ routes/incidents.js
@@ -3,9 +3,20 @@ const router = express.Router();
 const validateInput = require('../utils/validateInput');
 const passport = require('passport');
 const sql = require('../db/db');
-const keys = require('../utils/keys');
 
-router.get('/', (req, res) => {
+// @ACCESS - Public
+// @ENDPOINT - /api/incidents/all
+// @DESCRIPTION - Get all reported incidents
+router.get('/all', (req, res) => {
+  // Query the database, grab the incidents and send them out
+  res.send('Incidents route');
+});
+
+// @ACCESS - Private
+// @ENDPOINT - /api/incidents/add
+// @DESCRIPTION - Get all reported incidents
+router.post('/add', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // Check the input, and if valid, send it out to the database
   res.send('Incidents route');
 });
 
