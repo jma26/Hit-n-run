1941124f4753e2d9d85e33cda02fdb33039ae89c
Dennis Onder <webdev.dennisonder@gmail.com>
2018-11-14 16:23:40 +0100

    Moved spinner to its own component

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div id="spinner" className="spinner">
+      <p className="spinner__text">Swervie</p>
+        <span className="spinner__outer-span">
+          <div className="spinner__inner-span">
+        </div>
+      </span>
+    </div>
+  )
+}
+
+export default Spinner;
